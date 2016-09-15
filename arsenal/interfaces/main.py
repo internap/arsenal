5435fc7bd8c6f380c6a0c7049d994e8566ea35d7
Martin Roy <mroy@internap.com>
2016-09-15 16:12:47 -0400

    Add api wiring in the main

--- arsenal/interfaces/main.py
+++ arsenal/interfaces/main.py
@@ -1,3 +1,13 @@
+from arsenal.adapters.memory_datastore import MemoryDatastore
+from arsenal.core.manager import Manager
+from arsenal.interfaces.api import Api
 from flask import Flask
 
+
+def wire_stuff(app):
+    datastore = MemoryDatastore()
+    manager = Manager(datastore)
+    Api(app, manager)
+
 app = Flask(__name__)
+wire_stuff(app)
